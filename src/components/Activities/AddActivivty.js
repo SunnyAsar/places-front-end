e5de5ccb28ee9b15e980a9d4d032f37922c17fbc
Sunny Asar <ring2rings93@gmail.com>
2020-02-04 19:54:09 +0100

    add activites component

---
+++ src/components/Activities/AddActivivty.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+
+class AddActivity extends Component {
+  state = {}
+  render () {
+    return(
+      <div> 
+        <h1> Add Activity</h1>
+      </div>
+    )
+  }
+}
+
+export default AddActivity
